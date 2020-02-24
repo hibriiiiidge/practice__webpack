b27c737f593d495a8db20930ae8ca7cffd57e7c7
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-02-24 11:11:16 +0900

    webpackを用いてバンドルするようにした

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+const _ = require('lodash');
+
 function component() {
   const element = document.createElement('div');
   const array = ['Hello', 'webpack']
   element.innerHTML = _.join(array, ' ')
   return element;
 }
-
 
 document.body.appendChild(component());
