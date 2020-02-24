941591e53de4cd952f33202706a199e148c7ac54
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-02-24 11:21:02 +0900

    webpack-dev-serverを追加した

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 
 function component() {
   const element = document.createElement('div');
-  const array = ['Hello', 'webpack']
+  const array = ['Hello', 'webpack', '!!!']
   element.innerHTML = _.join(array, ' ')
   return element;
 }
