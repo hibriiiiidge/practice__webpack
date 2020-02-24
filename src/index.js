8022137c8ecaa16c9db98b3d67ce98fee7047f25
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-02-24 13:34:09 +0900

    Reactでrenderされるまでを実装した

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
-require('./style.css')
-require('./style.scss')
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const _ = require('lodash');
-const logo = require('./logo.png')
-
-function component() {
-  const element = document.createElement('div');
-  const array = ['Hello', 'webpack', '!!!']
-  element.innerHTML = _.join(array, ' ')
-  return element;
-}
-
-document.body.appendChild(component());
-document.body.classList.add('haikei');
-
-const image = new Image();
-image.src = logo
-document.body.appendChild(image);
+ReactDOM.render(
+  <div>Hello, React!</div>,
+  document.getElementById('root')
+)
