0a75f565c9209375092d69829a1e16823ae2c1ca
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-02-24 11:40:15 +0900

    Moduleについて実装した

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const _ = require('lodash');
+const utilities = require('./utilities');
+
+console.log(utilities.NAME);
 
 function component() {
   const element = document.createElement('div');
---
+++ src/utilities.js
@@ -0,0 +1,6 @@
+export function Nijou(num) {
+  return num**2;
+}
+
+
+export const NAME = 'yasu'
